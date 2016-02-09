144f07d38f5fbf542d387654993254a1c62333c8
kmcnee <kyle.mcnee@gmail.com>
2016-02-09 12:36:09 -0500

    Added cursor adapter

--- NeighborhoodGuide/app/src/main/java/com/example/kylemcnee/neighborhoodguide/SearchActivity.java
+++ NeighborhoodGuide/app/src/main/java/com/example/kylemcnee/neighborhoodguide/SearchActivity.java
@@ -1,10 +1,16 @@
 package com.example.kylemcnee.neighborhoodguide;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -14,7 +20,20 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         Cursor cursor = GloryholeOpenHelper.getInstance(SearchActivity.this).getGloryholeList();
+        CursorAdapter cursorAdapter = new CursorAdapter(SearchActivity.this, cursor,0) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false);
+            }
 
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView textView = (TextView)view.findViewById(android.R.id.text1);
+
+                //TODO How to display multiple values/columns
+                textView.setText(cursor.getString(cursor.getColumnIndex(GloryholeOpenHelper.COL_NAME)));
+            }
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
